131ee9a71544946271db86f730fdd14e4275b189
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-05-08 14:52:45 -0400

    able to get data from youtube

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
-
 const API_KEY = 'AIzaSyCiKG65yPmHZ3MGILUsjRTduad3CDOBmqk';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key:API_KEY, term: 'eric koston'}, function(data){
+			this.setState({ videos: data });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(
-	<App />,
-	document.querySelector('.container')
-	);
+ReactDOM.render( <App />, document.querySelector('.container') );
+
+//downwards data flow: 
