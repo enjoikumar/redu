f6059fc2b3f66d0d9ccee7a9d8a4df6ea0e2941d
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-05-08 15:42:12 -0400

    changed root name

---
+++ video_browser/src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyCiKG65yPmHZ3MGILUsjRTduad3CDOBmqk';
+import VideoList from './components/video_list';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key:API_KEY, term: 'eric koston'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos = {this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render( <App />, document.querySelector('.container') );
+
+//downwards data flow: 
